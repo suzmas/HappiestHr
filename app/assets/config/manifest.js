ea2186847ad9bb565965e9d05672eac6814e7630
Suzan Sucro <suzansucro@gmail.com>
2017-01-15 18:49:27 -0800

    Add some navbar styling from Classix template

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
+//= link_tree ../fonts
+//= link_tree ../fonts/line-icons
+//= link_tree ../line-icons/fonts
+
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
