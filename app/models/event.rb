a85747359b2903d20a7fda5bfbb89e0efac93bec
Suzan Sucro <suzansucro@gmail.com>
2017-01-21 13:40:51 -0800

    Add sorting methods to Events model. Add general splash page styling.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :bar
+
+  def day_of_week
+    days = { "Monday" => 0, "Tuesday" => 1, "Wednesday" => 2, "Thursday" => 3, "Friday" => 4, "Saturday" => 5, "Sunday" => 6 }
+
+    days[day]
+  end
+
+
+  def sorted
+    Event.all.sort {|event| event.day_of_week }
+  end
+
+  
 end
